962ab9f7ca36537ba3b77dd33a8c8965db21ce0d
giwoong <dev.choi0409@gmail.com>
2025-09-18 15:58:02 +0900

    Feat: 랭킹 조건 수정

--- src/main/java/com/leafup/leafupbackend/point/domain/repository/PointHistoryRepository.java
+++ src/main/java/com/leafup/leafupbackend/point/domain/repository/PointHistoryRepository.java
@@ -1,6 +1,7 @@
 package com.leafup.leafupbackend.point.domain.repository;
 
 import com.leafup.leafupbackend.point.application.dto.MonthlyPointAggregationDto;
+import com.leafup.leafupbackend.point.application.dto.PointAggregationDto;
 import com.leafup.leafupbackend.point.domain.PointHistory;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,9 +11,16 @@ import org.springframework.data.repository.query.Param;
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 
     @Query("SELECT new com.leafup.leafupbackend.point.application.dto.MonthlyPointAggregationDto(ph.member, SUM(ph.amount)) " +
-           "FROM PointHistory ph " +
-           "WHERE YEAR(ph.createdAt) = :year AND MONTH(ph.createdAt) = :month " +
-           "GROUP BY ph.member")
+            "FROM PointHistory ph " +
+            "WHERE YEAR(ph.createdAt) = :year AND MONTH(ph.createdAt) = :month " +
+            "GROUP BY ph.member")
     List<MonthlyPointAggregationDto> aggregateMonthlyPoints(@Param("year") int year, @Param("month") int month);
+
+    @Query("SELECT new com.leafup.leafupbackend.point.application.dto.PointAggregationDto(ph.member, SUM(ph.amount)) " +
+            "FROM PointHistory ph " +
+            "WHERE ph.amount > 0 " +
+            "GROUP BY ph.member " +
+            "ORDER BY SUM(ph.amount) DESC")
+    List<PointAggregationDto> findTotalPointsByMember();
 
 }
